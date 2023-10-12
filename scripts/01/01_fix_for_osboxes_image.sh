ca1ca0cd9f0667d1025919a7887ae6ffa5df598b
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2023-10-12 07:15:13 +0200

    fix for virtualbox image

---
+++ scripts/01/01_fix_for_osboxes_image.sh
@@ -0,0 +1,4 @@
+# the following is necessary when using the osboxes image for Ubuntu 22-10, as the repository has changed meanwhile
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak 
+sudo sed -i -re 's/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+sudo apt-get update
