2185ace811aeb8ed552fbe916a44c646bd763c61
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2024-10-03 17:28:54 +0200

    adapted filenames and updated description to clone WSL for 2nd cluster node

--- scripts/01/01a_fix_for_osboxes_image.sh
+++ scripts/01/01a_fix_for_osboxes_image.sh
@@ -1,4 +1,5 @@
 # the following is necessary when using the osboxes image for Ubuntu 22-10, as the repository has changed meanwhile
+# therefore only important, when working with VmWare or VirtualBox virtualisation and only with certain versions!
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak 
 sudo sed -i -re 's/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 sudo apt-get update
