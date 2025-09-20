23b8719a30cf552394f56d2a7fae478bc68a2416
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2025-09-20 21:50:09 +0200

    corrected port postfixes

--- scripts/01/01h_prepare_datanodes_in_openstack.sh
+++ scripts/01/01h_prepare_datanodes_in_openstack.sh
@@ -1,7 +1,7 @@
 #################################################################
 # Title:        01f_prepare_datanodes_in_openstack.sh
 # Description:  This is used for starting a student-specific datanode on central datanodes
-#               it has to be executed once per student and datanode              
+#               it has to be executed once per student and datanode (as user "hduser")            
 # Parameters:  
 #          $1:  port number postfix
 #          $2:  IP address of student-specific openstack namenode
@@ -13,7 +13,7 @@ let RetCode=0
 Usage () {
    echo "USAGE: `basename $0` <PortNumber> <NameNode-IP-Address>"
    echo "       Example: $0 3 10.77.16.124"
-   echo "       the value has to be the according numerical postfix of the datanode's name, e.g. for bigdataSWD03 use value '3'"
+   echo "       the value has to be the according numerical postfix of the datanode's name, e.g. for bigdataSWD03 use value '3', ITM students use 100+"
    echo "       This script has to be executed once on each additional datanode (beside the one, which runs on namenode) as user 'hduser'"
    echo "       if you have logged in from that client, you can try: `basename $0` 3 \$(echo \$SSH_CLIENT | cut -d' ' -f1)"
 }
@@ -24,7 +24,7 @@ if [ $# -lt $NumParams -o "$1" = "-?" -o "$1" = "--help" ]; then
 fi
 RetCode=0
 # the real code starts here
-PortPostfix=$(printf %02d $1)
+PortPostfix=$(printf %03d $1)
 NameNodeIP=$2
 if [ ! -n "$SSH_CLIENT" ]; then export SSH_CLIENT=$NameNodeIP; fi
 echo "creating environment for PortPostfix $PortPostfix and namenode $NameNodeIP"
@@ -85,15 +85,15 @@ cat >${HADOOP_CONF_DIR}/hdfs-site.xml <<EOF
    <!-- DataNode X Konfiguration (eigene Ports) -->
    <property>
       <name>dfs.datanode.address</name>
-      <value>0.0.0.0:501${PortPostfix}</value>
+      <value>0.0.0.0:51${PortPostfix}</value>
    </property>
    <property>
       <name>dfs.datanode.http.address</name>
-      <value>0.0.0.0:508${PortPostfix}</value>
+      <value>0.0.0.0:58${PortPostfix}</value>
    </property>
    <property>
       <name>dfs.datanode.ipc.address</name>
-      <value>0.0.0.0:502${PortPostfix}</value>
+      <value>0.0.0.0:52${PortPostfix}</value>
    </property>
 </configuration>
 EOF
@@ -101,7 +101,7 @@ EOF
 sed -i "s#/usr/local/hadoop/hadoopdata/hdfs/tmp#/usr/local/hadoop/hadoopdata/datanode${NameNodeIP}/tmp#" ${HADOOP_CONF_DIR}/core-site.xml
 
 echo "successfully prepared specific environment for Hadoop in ${HADOOP_CONF_DIR}"
-echo "Ports to use are: 501${PortPostfix}, 502${PortPostfix} and 508${PortPostfix}"
+echo "Ports to use are: 51${PortPostfix}, 52${PortPostfix} and 58${PortPostfix}"
 echo "datanodes are started by namenode and executing ~/datanode.env with specific config"
 source ~/datanode.env
 
