81f118107e5be0a09bad62778c6db32f3bed7bfd
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2025-11-22 17:58:34 +0100

    test call against postgres database on datanode1

---
+++ scripts/grp02/grp02_postgres_testcall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#################################################################
+# Title:        grp02_postgres_testcall.sh
+# Description:  test call against postgres DB in docker container     
+# Parameters:  
+#          $1:  username
+#################################################################
+
+let NumParams=1   # number of mandatory parameters
+let RetCode=0
+
+Usage () {
+   echo "USAGE: `basename $0` <username>"
+   echo "       Example: $0 swd00"
+   echo "   study in small letters and 2 digit current number"
+}
+#
+if [ $# -lt $NumParams -o "$1" = "-?" -o "$1" = "--help" ]; then
+   Usage;
+   exit 1;
+fi
+export UserName=$1
+export dbName=$UserName
+export schemaName=$UserName
+psql -h datanode1 -p 5432 -d $UserName -U $dbName <<!
+ SET search_path TO $schemaName, public;
+ CREATE OR REPLACE VIEW $schemaName.v_studentspostgres AS SELECT * FROM $schemaName.studentspostgres ORDER BY Last_Name DESC;
+ SELECT * from $schemaName.v_studentspostgres;
+!
