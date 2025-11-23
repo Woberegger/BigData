83e9409d5623c5fc4a096f43c474820e3acc1b54
Wolfgang OBEREGGER <Wolfgang.Oberegger@knapp.com>
2025-11-23 07:18:29 +0100

    mysql and postgres test calls working

---
+++ scripts/grp02/grp02_mysql_testcall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#################################################################
+# Title:        grp02_mysql_testcall.sh
+# Description:  test call against mysql DB in docker container     
+# Parameters:  
+#          $1:  username
+#          $2:  hostname, where docker container runs
+#################################################################
+
+let NumParams=1   # number of mandatory parameters
+let RetCode=0
+
+Usage () {
+   echo "USAGE: `basename $0` <username> [mySQL-Host]"
+   echo "       Example: $0 swd00 datanode1"
+   echo "   study in small letters and 2 digit current number"
+}
+#
+if [ $# -lt $NumParams -o "$1" = "-?" -o "$1" = "--help" ]; then
+   Usage;
+   exit 1;
+fi
+UserName=$1
+Passwd=$UserName
+dbHost=${2:-datanode1}
+dbName=$UserName
+
+echo "First connect to private test schema $UserName in database $dbName, which should be empty"
+mysql --ssl=FALSE -u $UserName -D $dbName -h $dbHost -P 13306 --password=$Passwd <<!
+   show tables;
+!
+
+UserName=${UserName}hive
+Passwd=$UserName
+dbName=$UserName
+
+echo "Then connect to Hive Metastore DB with user $UserName in database $dbName, which should find 83 tables, after 'schematool' for hive was executed"
+mysql --ssl=FALSE -u $UserName -D $dbName -h $dbHost -P 13306 --password=$Passwd <<!
+   show tables;
+!
--- scripts/grp02/grp02_postgres_testcall.sh
+++ scripts/grp02/grp02_postgres_testcall.sh
@@ -4,14 +4,15 @@
 # Description:  test call against postgres DB in docker container     
 # Parameters:  
 #          $1:  username
+#          $2:  hostname, where docker container runs
 #################################################################
 
 let NumParams=1   # number of mandatory parameters
 let RetCode=0
 
 Usage () {
-   echo "USAGE: `basename $0` <username>"
-   echo "       Example: $0 swd00"
+   echo "USAGE: `basename $0` <username> [Postgres-Host]"
+   echo "       Example: $0 swd00 datanode1"
    echo "   study in small letters and 2 digit current number"
 }
 #
@@ -20,9 +21,10 @@ if [ $# -lt $NumParams -o "$1" = "-?" -o "$1" = "--help" ]; then
    exit 1;
 fi
 export UserName=$1
+export dbHost=${2:-datanode1}
 export dbName=$UserName
 export schemaName=$UserName
-psql -h datanode1 -p 5432 -d $UserName -U $dbName <<!
+psql -h $dbHost -p 5432 -d $UserName -U $dbName <<!
  SET search_path TO $schemaName, public;
  CREATE OR REPLACE VIEW $schemaName.v_studentspostgres AS SELECT * FROM $schemaName.studentspostgres ORDER BY Last_Name DESC;
  SELECT * from $schemaName.v_studentspostgres;
